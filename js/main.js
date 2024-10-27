adedd4081b425bbdc096f8acd718fc3e684e2b01
Your Name <you@example.com>
2024-10-28 00:30:16 +0300

    edit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function openNav3() {
+    document.getElementById("mySidenavh").style.width = "75%";
+}
+function closeNavh() {
+    document.getElementById("mySidenavh").style.width = "0";
+    document.getElementById("mainh").style.marginLeft = "0";
+}
+function closeOffcanvas2() {
+    document.getElementById("myOffcanvas2").style.display = "none";
+}
+function openNav4() {
+    document.getElementById("mySidenav").style.width = "75%";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
